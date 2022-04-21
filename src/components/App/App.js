52669a41d819c60551b7865a3d5adb674da89a2a
Jamal Elden <64107698+jamalEden3@users.noreply.github.com>
2022-04-21 12:38:57 +0200

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import Header from "../Header";
+import Footer from "../Footer";
+
+function App() {
+  return (
+    <>
+      <h1>App</h1>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
